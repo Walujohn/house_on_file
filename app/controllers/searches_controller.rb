b0e3d81c71966285472f86232548828e1c562f93
jwharris215@gmail.com <jwharris215@gmail.com>
2023-02-04 22:09:51 -0500

    first draft the space results block

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,16 @@
 class SearchesController < ApplicationController
+  before_action :set_property
+    
   def index
+#    templates
+    @names = @property.list_of_space_names.select { |name| name == params[:query] }
+#    user created from the db
     @spaces = Space.containing(params[:query])
+  end
+    
+  private
+
+  def set_property
+    @property = current_group.properties.find(params[:property_id])
   end
 end
