d7a77f85a7060aada90140b4c98b27712e67ac58
jwharris215@gmail.com <jwharris215@gmail.com>
2023-03-25 11:45:16 -0400

    skypack dialog notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_135241) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_190602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_135241) do
     t.string "property_template"
     t.integer "interval"
     t.string "exclusion"
+    t.string "assigned"
     t.index ["group_id"], name: "index_properties_on_group_id"
   end
 
